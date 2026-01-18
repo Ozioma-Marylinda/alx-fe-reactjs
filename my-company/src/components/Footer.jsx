576524110094a38202cb8019cf95ce6b6c70f73c
Ozioma-Marylinda <oziomamarylinda@gmail.com>
2026-01-18 22:09:51 +0100

    Build a Simple Company Website with React

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', textAlign: 'center', marginTop: '40px' }}>
+      <p>© 2026 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
