b68e3a8265996f62b5d6c4cfc64f624069dcf75d
Ozioma-Marylinda <oziomamarylinda@gmail.com>
2026-02-20 03:45:02 +0100

    Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: [
     "./index.html",           // for Vite
     "./public/index.html",    // for CRA (safe to include)
-    "./src/**/*.{js,ts,jsx,tsx}",
+     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   darkMode: false, // or 'media' or 'class' if required
   theme: {
