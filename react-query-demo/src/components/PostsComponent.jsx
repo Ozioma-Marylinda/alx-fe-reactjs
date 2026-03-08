40d387895ec1b1d955c120aa7b7b4403a1b9ef0d
Ozioma-Marylinda <oziomamarylinda@gmail.com>
2026-03-08 20:10:53 +0100

    Advanced Data Handling with React Query

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,41 @@
+import { useQuery } from "react-query";
+
+function PostsComponent() {
+
+  const fetchPosts = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    return response.json();
+  };
+
+  const { data, isLoading, error, refetch } = useQuery(
+    "posts",
+    fetchPosts
+  );
+
+  if (isLoading) {
+    return <p>Loading posts...</p>;
+  }
+
+  if (error) {
+    return <p>Error fetching posts</p>;
+  }
+
+  return (
+    <div>
+
+      <h2>Posts</h2>
+
+      <button onClick={refetch}>Refetch Posts</button>
+
+      {data.map((post) => (
+        <div key={post.id} style={{ marginBottom: "20px" }}>
+          <h4>{post.title}</h4>
+          <p>{post.body}</p>
+        </div>
+      ))}
+
+    </div>
+  );
+}
+
+export default PostsComponent;
