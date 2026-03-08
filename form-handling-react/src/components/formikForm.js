db946cf5748b4c1ab3e8b7b88933dc7a66615fa2
Ozioma-Marylinda <oziomamarylinda@gmail.com>
2026-03-08 19:15:12 +0100

    Update formikForm.js

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,4 +1,3 @@
-
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
@@ -11,22 +10,13 @@ function FormikForm() {
   };
 
   const validationSchema = Yup.object({
-    username: Yup.string()
-      .required("Username is required"),
-
-    email: Yup.string()
-      .email("Invalid email format")
-      .required("Email is required"),
-
-    password: Yup.string()
-      .min(6, "Password must be at least 6 characters")
-      .required("Password is required")
+    username: Yup.string().required("Username is required"),
+    email: Yup.string().required("Email is required"),
+    password: Yup.string().required("Password is required")
   });
 
-  const onSubmit = (values, { resetForm }) => {
+  const onSubmit = (values) => {
     console.log(values);
-    alert("Registration Successful!");
-    resetForm();
   };
 
   return (
