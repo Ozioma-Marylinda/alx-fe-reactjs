eced5641f63d0e5ee094ebfc350c3a8d2528f493
Ozioma-Marylinda <oziomamarylinda@gmail.com>
2026-03-08 19:00:31 +0100

    Implementing Form Handling in React with Controlled Components and Formik

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,17 @@
+import RegistrationForm from "./components/RegistrationForm"
+import FormikForm from "./components/FormikForm.jsX"
+
+function App() {
+  
+  return (
+    <>
+      <div>
+      <h1>User Registration</h1>
+      <RegistrationForm />
+      <FormikForm />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ form-handling-react/src/components/FormikForm.jsx
@@ -0,0 +1,65 @@
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+function FormikForm() {
+
+  const initialValues = {
+    username: "",
+    email: "",
+    password: ""
+  };
+
+  const validationSchema = Yup.object({
+    username: Yup.string()
+      .required("Username is required"),
+
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required")
+  });
+
+  const onSubmit = (values, { resetForm }) => {
+    console.log(values);
+    alert("Registration Successful!");
+    resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+
+        <div>
+          <label>Username</label>
+          <Field type="text" name="username" />
+          <ErrorMessage name="username" component="div" />
+        </div>
+
+        <div>
+          <label>Email</label>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+        </div>
+
+        <div>
+          <label>Password</label>
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+        </div>
+
+        <button type="submit">Register</button>
+
+      </Form>
+    </Formik>
+  );
+}
+
+export default FormikForm;
